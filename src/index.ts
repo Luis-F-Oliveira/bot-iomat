ac2518b91b50b8cb162468eb1063144463f49d0f
Luis-F-Oliveira <luis.f.oliveira505141@outlook.com>
2024-05-22 14:21:15 -0300

    finalizando sistema de coleta de dados

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { chromium } from 'playwright'
-import { readJson } from './lib/typescript/readjson';
+import { readJson } from './lib/typescript/readjson'
+import * as fs from 'fs'
 
 interface ISavedData {
   order: string
@@ -38,6 +39,9 @@ class App
       .toLowerCase()
     }
 
+    const date = new Date()
+    const formattedDate = date.toLocaleDateString('pt-BR')
+
     const data = await readJson('./json/servers.json')
     const servers = data.servers
 
@@ -47,25 +51,47 @@ class App
 
     await page.locator('xpath=/html/body/div[2]/div[2]/div/div/div/div[4]/ul/li[1]/a[1]').click()
 
-    const matter: string[] = []
-    const text = await page.locator('xpath=/html/body/div[2]/div[3]/div/div/div/div').innerText()
-    const textProcessed = processText(text)
-    matter.push(textProcessed)
+    while (true) {
+      const a = page.locator('xpath=/html/body/div[2]/div[1]/div/div[1]/a')
+      const aText = await a.innerText()
+      if (aText.includes(formattedDate)) {
+        a.click()
+        await page.waitForTimeout(3000)
+        const matter: string[] = []
+        const text = await page.locator('xpath=/html/body/div[2]/div[3]/div/div/div/div').innerText()
+        const textProcessed = processText(text)
+        matter.push(textProcessed)
 
-    const foundNames = servers.filter((server: string) => 
-      matter.some((name: string) => name.includes(server))
-    )
+        const foundNames = servers.filter((server: string) => 
+          matter.some((name: string) => name.includes(server))
+        )
 
-    const order = await page.locator('xpath=/html/body/div[2]/div[3]/div/div/div/div/p[1]/span').innerText()
-    const url = page.url()
+        const order = await page.locator('xpath=/html/body/div[2]/div[1]/div/div[2]/h4').innerText()
+        const url = page.url()
 
-    this.savedData.push({ 
-      order: order,
-      url: url,
-      names: foundNames
+        this.savedData.push({ 
+          order: order,
+          url: url,
+          names: foundNames
+        })
+      } else {
+        break
+      }
+    }
+
+    this.savedData.forEach(obj => {
+      obj.names = obj.names.filter(name => name !== 'nan')
     })
 
-    console.log(this.savedData)
+    const jsonData = JSON.stringify(this.savedData, null, 2)
+
+    fs.writeFile('./json/savedData.json', jsonData, err => {
+      if (err) {
+        console.log('Erro ao salvar arquivo: ', err)
+        return
+      }
+      console.log('Arquivo salvo com sucesso!')
+    })
 
     await page.waitForTimeout(3000)
     await browser.close()
